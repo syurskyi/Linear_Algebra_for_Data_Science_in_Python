be217008c09fed1955aa8500b4f4399a44bbe1be
Grey <sergejyurskyj@yahoo.com>
2020-03-26 20:08:35 -0700

    Commited python files

---
+++ 2.5. Tensors.py
@@ -0,0 +1,56 @@
+# To add a new cell, type '# %%'
+# To add a new markdown cell, type '# %% [markdown]'
+# %% [markdown]
+# # Import the relevant libraries
+
+# %%
+import numpy as np
+
+# %% [markdown]
+# ## Creating a tensor
+
+# %%
+m1 = np.array([[5,12,6],[-3,0,14]])
+m1
+
+
+# %%
+m2 = np.array([[9,8,7],[1,3,-5]])
+m2
+
+
+# %%
+t = np.array([m1,m2])
+
+
+# %%
+t
+
+# %% [markdown]
+# ## Checking its shape
+
+# %%
+t.shape
+
+# %% [markdown]
+# ## Manually creating a tensor
+
+# %%
+t_manual = np.array([[[ 5, 12,  6], [-3,  0, 14]], [[ 9,  8,  7], [ 1,  3, -5]]])
+
+
+# %%
+t_manual
+
+
+# %%
+
+
+
+# %%
+
+
+
+# %%
+
+
